da588e6d45ab2ad24a82195f8cae6b733c4e6574
aparna <31291285+kameswari609@users.noreply.github.com>
2026-01-21 14:22:00 -0800

    Update default GCS bucket name
    Changed the default GCS bucket name to a unique value.

--- variables.tf
+++ variables.tf
@@ -32,7 +32,7 @@ variable "bq_dataset_name" {
 variable "gcs_bucket_name" {
   description = "My Storage Bucket Name"
   #Update the below to a unique bucket name
-  default     = "terraform-demo-terra-bucket"
+  default     = "module1-zoomcamp-bucket"
 }
 
 variable "gcs_storage_class" {
